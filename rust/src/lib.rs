54f6fdc71a898108a9203decf74ca9695e2a704b
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2026-06-25 09:36:38 +0200

    change file layout and fix rust to use python 3.14

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,39 +1,33 @@
-use numpy::{IntoPyArray, PyArray2};
-use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
+#[pyo3::pymodule]
+mod _native {
+    use numpy::{IntoPyArray, PyArray2, PyReadonlyArray2};
+    use pyo3::{
+        pyfunction,
+        Bound, Python,
+    };
 
-
-
-#[pymodule]
-fn _native(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    // The name of the module must match the package name
-    #[pyfn(m)]
+    #[pyfunction]
     fn merge_points_removing_overlap<'py>(
         py: Python<'py>,
-        connected_crop_rows: &PyArray2<f64>,
-        points_in_rows: &PyArray2<f64>,
-        row_information: &PyArray2<f64>,
+        connected_crop_rows: PyReadonlyArray2<f64>,
+        points_in_rows: PyReadonlyArray2<f64>,
+        row_information: PyReadonlyArray2<f64>,
         tolerance: f64,
         num_threads: usize
-    ) -> &'py PyArray2<f64> {
-        // Convert the input PyArray2 to ndarray::Array2
-        let c = connected_crop_rows.readonly().as_array().to_owned();
-        let p = points_in_rows.readonly().as_array().to_owned();
-        let r = row_information.readonly().as_array().to_owned();
-
+    ) -> Bound<'py, PyArray2<f64>> {
+        let c = connected_crop_rows.as_array().to_owned();
+        let p = points_in_rows.as_array().to_owned();
+        let r = row_information.as_array().to_owned();
         // Call the Rust function
-        let result = rust_fn::multithreaded_merging(&c, &p, &r, tolerance, num_threads);
-
+        let result = crate::rust_fn::multithreaded_merging(&c, &p, &r, tolerance, num_threads);
         result.into_pyarray(py)
     }
-    Ok(())
 }
 
 mod rust_fn {
-    use ndarray::{Array1, Array2, s};
-    use std::time::Instant;
+    use numpy::ndarray::{ViewRepr, OwnedRepr, Dim, ArrayBase, Axis, Array1, Array2, s};
     use rayon::prelude::*;
     use indicatif::{ProgressBar, ProgressStyle};
-    use ndarray::prelude::*;
     use std::sync::{Arc, Mutex};
 
     pub fn determine_points_relations(
@@ -41,17 +35,17 @@ mod rust_fn {
         row2: &Array1<f64>,
     ) -> (Array1<f64>, Array1<f64>, Array1<f64>, Array1<f64>) {
         /*
-        Given two lines with start and end point calculates which point on each line is 
+        Given two lines with start and end point calculates which point on each line is
         closest and farthest to the opposite line
         */
         // reshape skipping first element (row number)
-        let r1 = row1.slice(s![1..]).to_owned().into_shape((3, 2)).unwrap();
-        let r2 = row2.slice(s![1..]).to_owned().into_shape((3, 2)).unwrap();
+        let r1 = row1.slice(s![1..]).to_owned().into_shape_with_order((3, 2)).unwrap();
+        let r2 = row2.slice(s![1..]).to_owned().into_shape_with_order((3, 2)).unwrap();
 
         let mut max_dist = 0.0;
         let mut pair = (0, 0);
-        for (i, a) in r1.axis_iter(ndarray::Axis(0)).enumerate() {
-            for (j, b) in r2.axis_iter(ndarray::Axis(0)).enumerate() {
+        for (i, a) in r1.axis_iter(Axis(0)).enumerate() {
+            for (j, b) in r2.axis_iter(Axis(0)).enumerate() {
                 let d = ((a[0] - b[0]).powi(2) + (a[1] - b[1]).powi(2)).sqrt();
                 if d > max_dist {
                     max_dist = d;
@@ -60,7 +54,7 @@ mod rust_fn {
             }
         }
 
-        let row_1_far: ndarray::ArrayBase<ndarray::OwnedRepr<f64>, ndarray::Dim<[usize; 1]>> = r1.row(pair.0).to_owned();
+        let row_1_far: ArrayBase<OwnedRepr<f64>, Dim<[usize; 1]>> = r1.row(pair.0).to_owned();
         let row_2_far = r2.row(pair.1).to_owned();
         let row_1_close = if pair.0 == 0 { r1.row(1).to_owned() } else { r1.row(0).to_owned() };
         let row_2_close = if pair.1 == 0 { r2.row(1).to_owned() } else { r2.row(0).to_owned() };
@@ -91,7 +85,7 @@ mod rust_fn {
         rows: &Vec<Array1<f64>>,
         tolerance: f64,
     ) -> Vec<Array1<f64>> {
-        /* 
+        /*
         Removes points between the center and the end of a line if the center is on the line.
         */
         let selected_rows: Vec<Array1<f64>> = rows
@@ -100,7 +94,7 @@ mod rust_fn {
                 let x = r[2];
                 let y = r[3];
                 let distance = ((center[0] - line_end[0]).powi(2) + (center[1] - line_end[1]).powi(2)).sqrt();
-                
+
                 if distance < tolerance {
                     x == x && y == y
                 } else if center[0] < line_end[0] && center[1] < line_end[1] {
@@ -132,7 +126,7 @@ mod rust_fn {
     ) -> Array2<f64> {
         /*
         Merging the points in points_in_rows based on the information from connected_crop_rows.
-        This is handled in a multithreaded manner. 
+        This is handled in a multithreaded manner.
         */
 
 
@@ -169,31 +163,31 @@ mod rust_fn {
     }
 
     fn merge_points_removing_overlap(
-        points_in_rows: &ArrayBase<ndarray::OwnedRepr<f64>, Dim<[usize; 2]>>, 
-        row_information: &ArrayBase<ndarray::OwnedRepr<f64>, Dim<[usize; 2]>>, 
-        tolerance: f64, 
-        row_segment: ArrayBase<ndarray::ViewRepr<&f64>, Dim<[usize; 1]>>
-    ) -> Vec<ArrayBase<ndarray::OwnedRepr<f64>, Dim<[usize; 1]>>> {
+        points_in_rows: &ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>,
+        row_information: &ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>,
+        tolerance: f64,
+        row_segment: ArrayBase<ViewRepr<&f64>, Dim<[usize; 1]>>
+    ) -> Vec<ArrayBase<OwnedRepr<f64>, Dim<[usize; 1]>>> {
         /*
-        Takes a single row and removes points which overlap with other rows it is connected to it 
+        Takes a single row and removes points which overlap with other rows it is connected to it
         */
-        
+
         let tile_number = row_segment[1] as usize;
         let tile_row = row_segment[2] as usize;
-    
+
         let mut rows = points_in_rows
-            .axis_iter(ndarray::Axis(0))
+            .axis_iter(Axis(0))
             .filter(|r| r[0] as usize == tile_number && r[1] as usize == tile_row)
             .map(|r| r.to_owned())
             .collect::<Vec<_>>();
-    
+
         let row = row_information
-            .axis_iter(ndarray::Axis(0))
+            .axis_iter(Axis(0))
             .find(|r| r[0] as usize == tile_number && r[4] as usize == tile_row)
             .unwrap()
             .slice(s![4..])
             .to_owned();
-    
+
         for i in 0..4 {
             if row_segment[3 + i * 2].is_nan() {
                 break;
@@ -201,24 +195,24 @@ mod rust_fn {
             let connected_tile_n = row_segment[3 + i * 2];
             let connected_row_n = row_segment[4 + i * 2];
             let connected_row = row_information
-                .axis_iter(ndarray::Axis(0))
+                .axis_iter(Axis(0))
                 .find(|r| r[0] == connected_tile_n && r[4] == connected_row_n)
                 .unwrap()
                 .slice(s![4..])
                 .to_owned();
-    
-    
+
+
             let (row_1_far, _, row_1_close, row_2_close) = determine_points_relations(
                 &row,
                 &connected_row,
             );
-    
+
             let center_of_ends = (row_1_close.clone() + row_2_close) / 2.0;
-    
+
             let point_proj = point_projection_of_p_onto_line(&center_of_ends, &row_1_close, &row_1_far);
-    
+
             rows = remove_points_past_center(&point_proj, &row_1_close, &rows, tolerance);
-    
+
         }
         rows
     }
