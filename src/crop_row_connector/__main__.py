f4c0c67406e5296c71d929d987d82c864a9f6ef6
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2026-07-01 14:19:54 +0200

    fix: make docs able to get argparser

--- src/crop_row_connector/__main__.py
+++ src/crop_row_connector/__main__.py
@@ -6,86 +6,90 @@ from pathlib import Path
 
 from crop_row_connector.combine_crop_rows import CombineCropRows
 
-parser = argparse.ArgumentParser(description="Combine crop rows")
-parser.add_argument(
-    "path_row_information",
-    type=str,
-    help="Path to the csv file containing the general information af the crop rows "
-    "(tile number, tile position, row angle, row number, x1, y1, x2, y2 (start and end point of the row in the tile))",
-)
-parser.add_argument(
-    "path_points_in_rows",
-    type=str,
-    help="Path to the csv file containing each point in the crop rows and their amount of vegetation "
-    "(tile number, row number, x, y, vegetation)",
-)
-parser.add_argument(
-    "--output_path_connected_crop_rows",
-    default=None,
-    type=Path,
-    help="Path to the output file, containing which crop rows where connected and from which tiles"
-    "This is optional, if not provided, the connected crop rows will not be saved",
-)
-parser.add_argument(
-    "--output_path_vegetation_points",
-    default=None,
-    type=Path,
-    help="Path to the output file, containing the points of the vegetation in the crop rows"
-    "This is optional, if not provided, the vegetation points will not be saved",
-)
-parser.add_argument(
-    "--output_path_healthy_vegetation_segments",
-    default=None,
-    type=Path,
-    help="Path to the output file, containing the points of the healthy vegetation in the crop rows"
-    "This is optional, if not provided, the healthy vegetation segments will not be saved",
-)
-parser.add_argument(
-    "--output_path_unhealthy_vegetation_segments",
-    default=None,
-    type=Path,
-    help="Path to the output file, containing the points of the unhealthy vegetation in the crop rows"
-    "This is optional, if not provided, the unhealthy vegetation segments will not be saved",
-)
-parser.add_argument(
-    "--angle_tolerance",
-    default=0.1,
-    type=float,
-    help="Angle tolerance for two crop rows to be connected. Measured in radians.",
-)
-parser.add_argument(
-    "--distance_tolerance",
-    default=0.1,
-    type=float,
-    help="Distance tolerance for two crop rows to be connected. Measured in meters.",
-)
-parser.add_argument(
-    "--vegetation_threshold",
-    default=127,
-    type=float,
-    help="Vegetation threshold for a point to be considered healthy vegetation. Measured in grayscale values from 0 to 255.",
-)
-parser.add_argument(
-    "--min_unhealthy_vegetation_length",
-    default=0.1,
-    type=float,
-    help="Minimum length tolerance for unhealthy vegetation in the crop rows, to be considered as a segment of unhealthy vegetation. Measured in meters.",
-)
-parser.add_argument(
-    "--max_segment_length",
-    default=5,
-    type=float,
-    help="Maximum length of segments to be considered for vegetation classification. Measured in meters.",
-)
-parser.add_argument(
-    "--max_workers",
-    default=os.cpu_count(),
-    type=int,
-    help="Set the maximum number of workers. Default to number of cpus.",
-)
+
+def _get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Combine crop rows")
+    parser.add_argument(
+        "path_row_information",
+        type=str,
+        help="Path to the csv file containing the general information af the crop rows "
+        "(tile number, tile position, row angle, row number, x1, y1, x2, y2 (start and end point of the row in the tile))",
+    )
+    parser.add_argument(
+        "path_points_in_rows",
+        type=str,
+        help="Path to the csv file containing each point in the crop rows and their amount of vegetation "
+        "(tile number, row number, x, y, vegetation)",
+    )
+    parser.add_argument(
+        "--output_path_connected_crop_rows",
+        default=None,
+        type=Path,
+        help="Path to the output file, containing which crop rows where connected and from which tiles"
+        "This is optional, if not provided, the connected crop rows will not be saved",
+    )
+    parser.add_argument(
+        "--output_path_vegetation_points",
+        default=None,
+        type=Path,
+        help="Path to the output file, containing the points of the vegetation in the crop rows"
+        "This is optional, if not provided, the vegetation points will not be saved",
+    )
+    parser.add_argument(
+        "--output_path_healthy_vegetation_segments",
+        default=None,
+        type=Path,
+        help="Path to the output file, containing the points of the healthy vegetation in the crop rows"
+        "This is optional, if not provided, the healthy vegetation segments will not be saved",
+    )
+    parser.add_argument(
+        "--output_path_unhealthy_vegetation_segments",
+        default=None,
+        type=Path,
+        help="Path to the output file, containing the points of the unhealthy vegetation in the crop rows"
+        "This is optional, if not provided, the unhealthy vegetation segments will not be saved",
+    )
+    parser.add_argument(
+        "--angle_tolerance",
+        default=0.1,
+        type=float,
+        help="Angle tolerance for two crop rows to be connected. Measured in radians.",
+    )
+    parser.add_argument(
+        "--distance_tolerance",
+        default=0.1,
+        type=float,
+        help="Distance tolerance for two crop rows to be connected. Measured in meters.",
+    )
+    parser.add_argument(
+        "--vegetation_threshold",
+        default=127,
+        type=float,
+        help="Vegetation threshold for a point to be considered healthy vegetation. Measured in grayscale values from 0 to 255.",
+    )
+    parser.add_argument(
+        "--min_unhealthy_vegetation_length",
+        default=0.1,
+        type=float,
+        help="Minimum length tolerance for unhealthy vegetation in the crop rows, to be considered as a segment of unhealthy vegetation. Measured in meters.",
+    )
+    parser.add_argument(
+        "--max_segment_length",
+        default=5,
+        type=float,
+        help="Maximum length of segments to be considered for vegetation classification. Measured in meters.",
+    )
+    parser.add_argument(
+        "--max_workers",
+        default=os.cpu_count(),
+        type=int,
+        help="Set the maximum number of workers. Default to number of cpus.",
+    )
+    return parser
 
 
 def _main() -> None:
+    parser = _get_parser()
     args = parser.parse_args()
     ccr = CombineCropRows()
     ccr.angle_tolerance = args.angle_tolerance
